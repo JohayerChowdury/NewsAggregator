495001f913184cdc9d4fdbd7cc2cfa08308a4c47
JohayerChowdury <ashik2001@hotmail.ca>
2025-01-21 23:49:27 -0500

    add package versions to make sure installed correctly

--- run.py
+++ run.py
@@ -24,6 +24,7 @@ RSS_FEEDS = {
     # "CBC News": "https://rss.cbc.ca/lineup/topstories.xml" # unsure
 }
 
+# TODO: stay away from company websites (have sales objectives that have "vendor" stuff)
 WEBSITES = {
     # "Accessorry Dwelling Finance News": "https://accessorydwellings.org/category/news/financing-news/",
     # "Flexobuild News": "https://flexobuild.com/media" # not works
