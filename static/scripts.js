637dd977f2955e03e6591897a2da3ab1dfbf101a
JohayerChowdury <ashik2001@hotmail.ca>
2025-01-26 13:56:12 -0500

    move scripts to seperate file

---
+++ static/scripts.js
@@ -0,0 +1,55 @@
+// for sources dialog
+const openSourcesDialogButton = document.getElementById("openSourcesDialog");
+const closeDialogSourcesButton = document.getElementById("closeSourcesDialog");
+const filterSourcesDialog = document.getElementById("filterSourcesDialog");
+
+openSourcesDialogButton.addEventListener("click", () => {
+  filterSourcesDialog.showModal();
+});
+
+closeDialogSourcesButton.addEventListener("click", () => {
+  filterSourcesDialog.close();
+});
+
+// for queries description dialog
+const openQueriesDialogButton = document.getElementById("openQueriesDescriptionDialog");
+const closeDialogQueriesButton = document.getElementById("closeQueriesDescriptionDialog");
+const queriesDescriptionDialog = document.getElementById("queriesDescriptionDialog");
+
+openQueriesDialogButton.addEventListener("click", () => {
+  queriesDescriptionDialog.showModal();
+});
+
+closeDialogQueriesButton.addEventListener("click", () => {
+  queriesDescriptionDialog.close();
+});
+
+// Sorting by date
+document.getElementById("sortDateAsc").addEventListener("click", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set("sort_date", "asc");
+  urlParams.delete("sort_title"); // Clear other sorting parameters
+  window.location.search = urlParams.toString();
+});
+
+document.getElementById("sortDateDesc").addEventListener("click", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set("sort_date", "desc");
+  urlParams.delete("sort_title"); // Clear other sorting parameters
+  window.location.search = urlParams.toString();
+});
+
+// Sorting by title
+document.getElementById("sortTitleAsc").addEventListener("click", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set("sort_title", "asc");
+  urlParams.delete("sort_date"); // Clear other sorting parameters
+  window.location.search = urlParams.toString();
+});
+
+document.getElementById("sortTitleDesc").addEventListener("click", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set("sort_title", "desc");
+  urlParams.delete("sort_date"); // Clear other sorting parameters
+  window.location.search = urlParams.toString();
+});
